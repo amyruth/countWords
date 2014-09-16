9afba2f710357bdfc7dca00e7a6f50ed66d37f16
amyruth <anrutherford@gmail.com>
2014-09-16 05:21:42 -0400

    separate string preprocessing into a separate function

--- countWords.js
+++ countWords.js
@@ -1,19 +1,25 @@
-var countWords = function(string){
-	string = string.toLowerCase();
+var remPunctuation = function(string){
+  string = string.toLowerCase();
   string = string.split("");
   
-	for(var i = 0; i < string.length; i+=1){
-	    if(string[i] === "," || string[i] === "."){
-	      string.splice(i,1);
+  for(var i = 0; i < string.length; i+=1){
+      if(string[i] === "," || string[i] === "."){
+        string.splice(i,1);
       }else if(string[i] === "!" || string[i] === "?"){
         string.splice(i,1);
       }else if(string[i] === ";" || string[i] === ":"){
         string.splice(i,1);
       }
-  	}
+    }
   string = string.join("");
   return string;
 };
 
 
-countWords("This, is the Most; difficult? army to command in my opinion!");
+var countWords = function(string){
+  string = remPunctuation(string);
+    return string;
+};
+
+
+countWords("This, is a Most; difficult? ARMY to command in my opinion!");
