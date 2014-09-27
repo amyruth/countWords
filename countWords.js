12a39221f58c687d954941fa002330a44263c961
amyruth <anrutherford@gmail.com>
2014-09-27 00:09:16 -0400

    sort and print object code complete

--- countWords.js
+++ countWords.js
@@ -9,12 +9,23 @@ var remPunctuation = function(string){
         string.splice(h,1, "");
       }else if(string[h] === ";" || string[h] === ":"){
         string.splice(h,1, "");
+      }else if(string[h] === "(" || string[h] === ")"){
+        string.splice(h,1,"");
       }
     }
   string = string.join("");
   return string;
 };
 
+var objectToArray = function(obj){
+  var sortedObj = [];
+  for(each in obj){
+    sortedObj.push([each, obj[each]]);
+  }
+  sortedObj = sortedObj.sort(function(a,b) {return a[1] - b[1]});
+  sortedObj.reverse();
+  return sortedObj;
+};
 
 var countWords = function(string){
   var scores = {};
@@ -36,11 +47,9 @@ var countWords = function(string){
     count = 0;
   }
 
-  for(each in scores){
-    sortWords.push([each, scores[each]]);
-  }
-  
+ finalCount = objectToArray(scores);
+ 
+ for( var k = 0; k < finalCount.length; k +=1){
+  console.log(finalCount[k][0] + " : " + finalCount[k][1]);
+ }
 };
-
-
-countWords("why must there be a there in there!!!");
